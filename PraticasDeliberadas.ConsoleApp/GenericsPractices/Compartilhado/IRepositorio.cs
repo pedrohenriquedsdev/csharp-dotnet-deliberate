96a70a28a1684fcb7fc73013dfb8ca6fa226b11e
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-06-08 08:03:23 -0300

    docs: add comments based on interface behavior

--- PraticasDeliberadas.ConsoleApp/GenericsPractices/Compartilhado/IRepositorio.cs
+++ PraticasDeliberadas.ConsoleApp/GenericsPractices/Compartilhado/IRepositorio.cs
@@ -1,6 +1,6 @@
 namespace PraticasDeliberadas.ConsoleApp;
 
-public interface IRepositorio<T> where T : EntidadeBase
+public interface IRepositorio<T> where T : EntidadeBase //todos que implementam essa Interface devem possuir implmentações de todos os seus comportamentos
 {
     void Cadastrar(T entidade);
     List<T> SelecionarTodos();
